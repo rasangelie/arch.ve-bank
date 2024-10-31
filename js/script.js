e6c01cb2e09ee20e8e51a9520d48a1f14d851af1
rasangelie <angeliebadar1@gmail.com>
2024-10-31 14:53:00 +0800

    feat: display movements, ins, and outs

--- js/script.js
+++ js/script.js
@@ -37,7 +37,12 @@ const movementsType = document.querySelector(".movements__type");
 const movementsDetail = document.querySelector(".movements__details");
 const movementsAmount = document.querySelector(".movements__amount");
 
-/**In, Out, Interest, Sort */
+/**In, Out, Interest */
+const summaryIn = document.querySelector(".summary__total--in");
+const summaryOut = document.querySelector(".summary__total--out");
+const summaryInterest = document.querySelector(".summary__total--interest");
+
+/**Transactions */
 const transferTo = document.getElementById("transfer-to").value;
 const transferAmount = Number(document.getElementById("transfer-amount").value);
 const requestAmount = Number(document.getElementById("request-amount").value);
@@ -54,7 +59,7 @@ const displayMovements = (mov) => {
   mov.forEach((mov, i) => {
     // console.log(`${curr}, ${index}`);
 
-    const type = mov > 0 ? "Deposit" : "Withdrawal";
+    const type = mov > 0 ? "deposit" : "withdrawal";
     const message =
       mov > 0
         ? "You deposited money successfully."
@@ -63,7 +68,9 @@ const displayMovements = (mov) => {
     <div class="movements__row">
           <div class="movements__flex">
             <div class="movements__left-flex">
-              <div class="movements__type">${i + 1} ${type}</div>
+              <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type.toUpperCase()}</div>
               <div class="movements__date">10/21/2024</div>
             </div>
 
@@ -82,3 +89,54 @@ const displayMovements = (mov) => {
 };
 
 displayMovements(account1.movements);
+
+//Display overall balance
+const calcDisplayBalance = (mov) => {
+  const balance = mov.reduce((acc, mov) => {
+    return acc + mov;
+  }, 0);
+
+  overallBalance.innerHTML = `PHP ${balance}`;
+};
+
+calcDisplayBalance(account1.movements);
+
+//Creating username
+const createUsername = (acc) => {
+  acc.forEach((acc) => {
+    acc.username = acc.owner.toLowerCase().split(" ")[0];
+  });
+};
+
+createUsername(accounts);
+// console.log(accounts);
+
+//Calculating IN,OUT, INTEREST
+
+const summaryCalculation = (mov) => {
+  //Ins
+  const ins = mov
+    .filter((mov) => {
+      return mov > 0;
+    })
+    .reduce((acc, mov) => {
+      return acc + mov;
+    }, 0);
+
+  summaryIn.innerHTML = `PHP ${ins}`;
+
+  //Out
+  const out = mov
+    .filter((mov) => {
+      return mov < 0;
+    })
+    .reduce((acc, mov) => {
+      return acc + mov;
+    }, 0);
+
+  summaryOut.innerHTML = `PHP ${out}`;
+
+  //Interest
+};
+
+summaryCalculation(account1.movements);
